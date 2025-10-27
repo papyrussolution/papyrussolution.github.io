fa9cd020496d4e73bfb9ac5c80c29278e6cc9a26
papyrussolution <papyrussolution@gmail.com>
2025-10-27 17:56:28 +0300

    prod-2-2

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,10 @@ const config = {
 	kit: { // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({pages: 'build', assets: 'build', fallback: '404.html'})
+		adapter: adapter({pages: 'build', assets: 'build', fallback: '404.html'}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '' : '',
+		}
 	},
 	extensions: ['.svelte', '.svx']
 };
